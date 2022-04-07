5d836b8dc1c8ec7eb9e80e7192e561a6473744dc
choumanhussein <chouman.hussein7@gmail.com>
2022-04-07 00:57:45 +0000

    Init

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+--- add to `app/assets/config/manifest.js`
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+//= link graphiql/rails/application.css
+//= link graphiql/rails/application.js
